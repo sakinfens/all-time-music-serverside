f13dc796075908c6483894ae56df7fdf2e3df107
sakinfens <sakinfens@gmail.com>
2021-05-07 19:00:50 -0400

    fixed port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
-const PORT = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 const server = new ApolloServer({
   playground: true,
